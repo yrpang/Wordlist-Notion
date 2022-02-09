e085eb17c979870e9e655b2399abcc26f70443ce
yrpang <yrpang@outlook.com>
2022-02-09 11:33:37 +0800

    basic func

--- index.js
+++ index.js
@@ -1,41 +1,115 @@
 import { Client } from "@notionhq/client";
+import axios from "axios";
+import { randomUUID, createHash } from 'crypto'
 
 const NOTION_KEY = 'secret_eVqnnBDiCYHtLU5mfvRLg48xq4nLnZpoFIBiy0RBIuE';
+const databaseId = '5fe6198ed109423b83b32b807c831e78';
+const APPKEY = '721e2adadc3e05f8';
+const key = '0MgIpFOAnVQHB7rmVwI8YBRNj47OBHGT';
 
 const notion = new Client({ auth: NOTION_KEY });
 
-const databaseId = '5fe6198ed109423b83b32b807c831e78';
+function truncate(q) {
+    var len = q.length;
+    if (len <= 20) return q;
+    return q.substring(0, 10) + len + q.substring(len - 10, len);
+}
 
-async function addItem(text) {
+async function getWordTrans(word) {
+    try {
+        const salt = randomUUID();
+        const curtime = Math.round(new Date().getTime() / 1000);
+        const hash = createHash('sha256')
+        const input = truncate(word);
+        const sign = hash.update(APPKEY + input + salt + curtime + key).digest("hex");
+        const reqUrl = 'https://openapi.youdao.com/api?q=' + word
+            + '&from=en&to=zh-CHS'
+            + '&appKey=' + APPKEY
+            + '&salt=' + salt
+            + '&sign=' + sign
+            + '&signType=v3'
+            + '&curtime=' + curtime;
+        const res = await axios.get(reqUrl);
+        const { errorCode, basic, isWord } = res.data;
+        if (errorCode != 0) {
+            return {
+                err: errorCode
+            }
+        }
+
+        if (isWord) {
+            const { phonetic, explains } = basic;
+            return {
+                err: errorCode,
+                isWord,
+                phonetic,
+                trans: explains.join()
+            }
+        } else {
+            const { translation } = res.data;
+            return {
+                err: errorCode,
+                isWord,
+                trans: translation.join()
+            }
+        }
+
+    } catch (e) {
+        console.error(e);
+        return {
+            err: -1,
+            errmsg: '请求失败'
+        }
+    }
+}
+
+async function addItem(src, res) {
+    const { isWord, trans, phonetic } = res
+    const basic_prop = {
+        'Word': {
+            title: [
+                {
+                    "text": {
+                        "content": src
+                    }
+                }
+            ]
+        },
+        'Meaning': {
+            rich_text: [
+                {
+                    "text": {
+                        content: trans
+                    }
+                }
+            ]
+        }
+    };
+    const properties = isWord ? {
+        ...basic_prop,
+        'Phonetic': {
+            rich_text: [
+                {
+                    "text": {
+                        content: '| ' + phonetic + ' |'
+                    }
+                }
+            ]
+        }
+    } : basic_prop;
+
     try {
         const response = await notion.pages.create({
             parent: { database_id: databaseId },
-            properties: {
-                title: {
-                    title: [
-                        {
-                            "text": {
-                                "content": text
-                            }
-                        }
-                    ]
-                },
-                'xGVJ': {
-                    rich_text: [
-                        {
-                            "text": {
-                                content: "12121"
-                            }
-                        }
-                    ]
-                }
-            },
+            properties
         });
-        console.log(response);
-        console.log("Success! Entry added.");
+        // console.log(response);
+        // console.log("Success! Entry added.");
     } catch (error) {
         console.error(error.body);
     }
 }
-
-addItem("Yurts in Big Sur, California");
+const word = 'connect';
+const trans = await getWordTrans(word);
+addItem(word, trans)
+// console.log(a);
