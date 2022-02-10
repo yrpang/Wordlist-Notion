46c47115af2f31a2bb152a4222fc64e5e6122528
yrpang <yrpang@outlook.com>
2022-02-10 15:54:45 +0800

    add keys template

---
+++ src/keys.js
@@ -0,0 +1,13 @@
+// Just a template
+const K = {
+    // Notion API
+    NOTION_KEY: '',
+    databaseId: '',
+    // youdao API
+    APPKEY: '',
+    key: '',
+    // api密码
+    PASSWD: ''
+};
+
+export { K };
