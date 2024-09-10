d809745b2eaef7e4e49e7a529bce4e3669e11fd9
yrpang <7457280+yrpang@users.noreply.github.com>
2024-09-10 12:55:03 +0800

    query

--- src/app.js
+++ src/app.js
@@ -3,11 +3,69 @@ import { TranslationAPI, NotionAPI } from './wordlist.js'
 import { APIError } from './error.js';
 import axios from 'axios';
 import { CONFIG } from './config.js';
+import { AzureChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import {
+    ChatPromptTemplate,
+    FewShotChatMessagePromptTemplate,
+} from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const llm = new AzureChatOpenAI({
+    model: 'gpt-4o-mini',
+    azureOpenAIApiKey: 'bb1887226cb94f1eb25a4e9a4a2cb8c0',
+    azureOpenAIApiInstanceName: 'chat-openai-api-start',
+    azureOpenAIApiDeploymentName: 'gpt-4o-mini',
+    azureOpenAIApiVersion: '2024-02-15-preview',
+});
+
+const examplePrompt = ChatPromptTemplate.fromMessages([
+    ["human", `Please tell me the Chinese meaning of words or phrases "{word}" in sentence "{sentence}".`],
+    ["ai", "{answer}"],
+]);
+const examples = [
+    {
+        word: "dictionary",
+        sentence: "Each example should be a dictionary representing an example input to the formatter prompt we defined above.",
+        answer: "字典"
+    },
+]
+const fewShotPrompt = new FewShotChatMessagePromptTemplate({
+    examplePrompt,
+    examples,
+    inputVariables: [], // no input variables
+});
+const finalPrompt = ChatPromptTemplate.fromMessages([
+    ["system", "You are a word translation assistant and can help users explain the meaning of words in a given sentence. You will only provide the meaning of the specified word, nothing else."],
+    fewShotPrompt,
+    ["human", `Please tell me the Chinese meaning of words or phrases "{word}" in sentence "{sentence}".`],
+]);
+
+const parser = new StringOutputParser();
 
 const { YOUDAO_ID, YOUDAO_TOKEN, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET } = CONFIG;
 
 const app = express()
 const port = 9000
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // 允许所有来源
+    res.header('Access-Control-Allow-Methods', 'GET'); // 允许的方法
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // 允许的请求头
+    next();
+});
+
+app.get('/query', async (req, res) => {
+    const { word, sentence } = req.query;
+
+    const chain = finalPrompt.pipe(llm).pipe(parser)
+    const ret = await chain.invoke({
+        word: word,
+        sentence: sentence
+    })
+
+    res.json(ret)
+})
 
 app.get('/api', async (req, res) => {
     const { token, database_id, word } = req.query;
@@ -100,7 +158,7 @@ app.get('/callback', async (req, res) => {
         const response = await axios.post('https://api.notion.com/v1/oauth/token', {
             'grant_type': 'authorization_code',
             'code': code,
-            'redirect_uri': 'https://wordlist.lnception.cn/callback'
+            'redirect_uri': 'https://1254913510-jpn35u72oj.ap-guangzhou.tencentscf.com/callback'
         }, {
             auth: {
                 username: OAUTH_CLIENT_ID,
--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 // Just a template
 const CONFIG = {
     // Notion
-    OAUTH_CLIENT_ID: '',
-    OAUTH_CLIENT_SECRET: '',
+    OAUTH_CLIENT_ID: '2cb2df41-a063-4460-b2aa-5d3ed39b4f73',
+    OAUTH_CLIENT_SECRET: 'secret_ruToKjRH1pUcDoIoTzYaqPqBBEDPbWgEydIzSxB20E2',
     // youdao API
-    YOUDAO_ID: '',
-    YOUDAO_TOKEN: ''
+    YOUDAO_ID: '721e2adadc3e05f8',
+    YOUDAO_TOKEN: 'zsMiLJQNXBxk18XvZI0pXAUUYFDhmNhD'
 };
 
 export { CONFIG };
